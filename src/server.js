5d72eeb372e6d62b29ddec0d2a588b208ee76b4d
uhlariks <sarah.uhlarik@gmail.com>
2021-06-20 12:41:35 -0500

    comments endpoint

--- src/server.js
+++ src/server.js
@@ -60,9 +60,25 @@ app.post("/api/articles/:name/add-comment", (req, res) => {
   const { username, text } = req.body;
   const articleName = req.params.name;
 
-  articlesInfo[articleName].comments.push({ username, text });
+  withDB(async (db) => {
+    const articleInfo = await db
+      .collection("articles")
+      .findOne({ name: articleName });
+    await db.collection("articles").updateOne(
+      { name: articleName },
+      {
+        $set: {
+          comments: articleInfo.comments.concat({ username, text }),
+        },
+      }
+    );
 
-  res.status(200).send(articlesInfo[articleName]);
+    const updatedArticleInfo = await db
+      .collection("articles")
+      .findOne({ name: articleName });
+
+    res.status(200).json(updatedArticleInfo);
+  }, res);
 });
 
 app.listen(8000, () => console.log("Listening on port 8000"));

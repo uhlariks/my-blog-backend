627a5cb608b39ae4f9edc5a062d1e8ab4d688686
uhlariks <sarah.uhlarik@gmail.com>
2021-06-20 12:30:57 -0500

    cleaning up code

--- src/server.js
+++ src/server.js
@@ -6,34 +6,35 @@ const app = express();
 
 app.use(bodyParser.json());
 
-app.get("/api/articles/:name", async (req, res) => {
+const withDB = async (operations, res) => {
   try {
-    const articleName = req.params.name;
-
     const client = await MongoClient.connect("mongodb://localhost:27017", {
       useNewUrlParser: true,
     });
     const db = client.db("my-blog");
 
-    const articleInfo = await db
-      .collection("articles")
-      .findOne({ name: articleName });
-    res.status(200).json(articleInfo);
+    await operations(db);
 
     client.close();
   } catch (error) {
     res.status(500).json({ message: "Error connecting to db", error });
   }
+};
+
+app.get("/api/articles/:name", async (req, res) => {
+  withDB(async (db) => {
+    const articleName = req.params.name;
+
+    const articleInfo = await db
+      .collection("articles")
+      .findOne({ name: articleName });
+    res.status(200).json(articleInfo);
+  }, res);
 });
 
 app.post("/api/articles/:name/upvote", async (req, res) => {
-  try {
+  withDB(async (db) => {
     const articleName = req.params.name;
-
-    const client = await MongoClient.connect("mongodb://localhost:27017", {
-      useNewUrlParser: true,
-    });
-    const db = client.db("my-blog");
 
     const articleInfo = await db
       .collection("articles")
@@ -52,11 +53,7 @@ app.post("/api/articles/:name/upvote", async (req, res) => {
       .findOne({ name: articleName });
 
     res.status(200).json(updatedArticleInfo);
-
-    client.close();
-  } catch (error) {
-    res.status(500).json({ message: "Error connecting to db", error });
-  }
+  }, res);
 });
 
 app.post("/api/articles/:name/add-comment", (req, res) => {
